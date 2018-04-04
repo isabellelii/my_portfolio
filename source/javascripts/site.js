4be5051bcb52e246c2deee50023d58bdbf92acdb
Isabelle <isabellelidberg@live.se>
2018-04-04 21:57:43 +0200

    adds timeline and new navbar

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -1,10 +1,3 @@
-// $(document).ready(function() {
-//   $("img").click(function(){
-//       $("img").animate({
-//           left: '250px',
-//           opacity: '0.5',
-//           height: '150px',
-//           width: '150px'
-//       });
-//   });
-//     });
+$(document).ready(function() {
+
+    });
